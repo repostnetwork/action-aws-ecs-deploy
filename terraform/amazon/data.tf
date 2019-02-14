6647766e29c665086b082a33e5adc0c4900b2d76
seandunn <seantd@live.com>
2019-02-14 15:55:14 -0800

    Refactored data into its own file. Replaced local variables for ecr with a resource.

---
+++ terraform/amazon/data.tf
@@ -0,0 +1,26 @@
+data "aws_availability_zones" "available" {}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_subnet" "default" {
+  count = "${length(data.aws_subnet_ids.default.ids)}"
+  id    = "${data.aws_subnet_ids.default.ids[count.index]}"
+}
+
+data "aws_ecs_cluster" "main" {
+  cluster_name = "${var.cluster_name}"
+}
+
+data "aws_iam_role" "task_container_role" {
+  name = "${var.ecs_task_container_role}"
+}
+
+data "aws_iam_role" "task_execution_role" {
+  name = "${var.ecs_task_execution_role}"
+}
