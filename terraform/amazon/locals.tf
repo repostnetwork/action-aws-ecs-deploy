f7bf73950b754968fd53204c417e532fff3d62d4
seandunn <seantd@live.com>
2019-02-22 15:55:42 -0800

    Fixed cname logic

--- terraform/amazon/locals.tf
+++ terraform/amazon/locals.tf
@@ -1,6 +1,6 @@
 locals {
   logical_name = "${replace(var.github_repository, "repostnetwork/", "")}"
   domain_name = "${var.env == "production" ? "repostnetwork.com" : "repostnetworktesting.com"}"
-  aws_route53_zone_name = "${concat(local.domain_name, ".")}"
-  aws_route53_record_name = "${concat(local.logical_name, ".services.", local.domain_name)}"
+  aws_route53_zone_name = "${local.domain_name}."
+  aws_route53_record_name = "${local.logical_name}.services.${local.domain_name}"
 }
