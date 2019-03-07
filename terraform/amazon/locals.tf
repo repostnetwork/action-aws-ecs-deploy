e68eba139804fc425b58443f7acf9a5b2d65180a
seandunn <seantd@live.com>
2019-03-07 11:59:20 -0800

    Adjust deployment healthy percent according to the container count

--- terraform/amazon/locals.tf
+++ terraform/amazon/locals.tf
@@ -2,4 +2,6 @@ locals {
   domain_name = "${var.env == "production" ? "repostnetwork.com" : "repostnetworktesting.com"}"
   aws_route53_zone_name = "${local.domain_name}."
   aws_route53_record_name = "${var.logical_name}.services.${local.domain_name}"
+  min_healthy_percent = "${var.container_count * 100}"
+  max_healthy_percent = "${var.container_count * 200}"
 }
