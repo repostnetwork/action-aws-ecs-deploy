b855e11affda0c17b14578feae8530f88d51c40b
Jonathan Shin <jonathan.shin@soundcloud.com>
2021-03-16 13:34:15 -0700

    Updates sd config to use correct naming convention

--- terraform/amazon/service-discovery.tf
+++ terraform/amazon/service-discovery.tf
@@ -1,13 +1,13 @@
 resource "aws_service_discovery_private_dns_namespace" "internal_dns" {
   name        = "service.${var.env}"
   description = "Service Discovery for Internal Services"
-  vpc         = data.aws_vpc.default.id
+  vpc         = "${data.aws_vpc.default.id}"
 }
 
 resource "aws_service_discovery_service" "internal" {
   name = substr(var.logical_name, 0, min(length(var.logical_name), 32))
   dns_config {
-    namespace_id = aws_service_discovery_private_dns_namespace.internal_dns.id
+    namespace_id = "${aws_service_discovery_private_dns_namespace.internal_dns.id}"
 
     dns_records {
       ttl  = 10
