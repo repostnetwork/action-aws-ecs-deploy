90d55fec00565a889e58878eb76f850cbad0688d
Jonathan Shin <jonathan.shin@soundcloud.com>
2021-03-16 13:26:30 -0700

    Fixes Indent

--- terraform/amazon/service-discovery.tf
+++ terraform/amazon/service-discovery.tf
@@ -1,11 +1,11 @@
 resource "aws_service_discovery_private_dns_namespace" "internal_dns" {
   name        = "service.${var.env}"
   description = "Service Discovery for Internal Services"
-  vpc         = "${data.aws_vpc.default.id}"
+  vpc         = data.aws_vpc.default.id
 }
 
 resource "aws_service_discovery_service" "internal" {
-  name = "${substr(var.logical_name, 0, min(length(var.logical_name), 32))}"
+  name = substr(var.logical_name, 0, min(length(var.logical_name), 32))
   dns_config {
     namespace_id = aws_service_discovery_private_dns_namespace.internal_dns.id
 
