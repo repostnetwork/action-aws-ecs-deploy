79ab3042d429ccadbf4f7e21eaa2011adf85796d
Jonathan Shin <jonathan.shin@soundcloud.com>
2021-03-17 13:23:39 -0700

    Uses CIDR block config

--- terraform/amazon/security-groups.tf
+++ terraform/amazon/security-groups.tf
@@ -50,10 +50,8 @@ resource "aws_security_group" "ecs_tasks" {
     protocol = "tcp"
     from_port = "${var.port}"
     to_port = "${var.port}"
-    security_groups = [
-      "${aws_security_group.lb.id}",
-      "${data.aws_vpc.default.cidr_block}"
-      ]
+    security_groups = ["${aws_security_group.lb.id}"]
+    cidr_block = ["${data.aws_vpc.default.cidr_block}"]
   }
 
   egress {
