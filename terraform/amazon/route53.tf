71a94d5d6941b73593d5c823082458542521c7a6
Xartrick <nick.martinez@soundcloud-externals.com>
2021-09-09 17:06:07 +0200

    Update Terraform
    - Update Terraform to 0.13
    - Update AWS provider to 3.57

--- terraform/amazon/route53.tf
+++ terraform/amazon/route53.tf
@@ -1,8 +1,10 @@
 resource "aws_route53_record" "main" {
-  count = "${var.is_worker ? 0 : 1}" # no cname if worker
-  zone_id = "${data.aws_route53_zone.selected.zone_id}"
-  name    = "${local.aws_route53_record_name}"
+  count   = var.is_worker ? 0 : 1 # no cname if worker
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = local.aws_route53_record_name
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_alb.main.dns_name}"]
+  records = [
+    aws_alb.main.dns_name
+  ]
 }
--- terraform/amazon/security-groups.tf
+++ terraform/amazon/security-groups.tf
@@ -1,30 +1,30 @@
 resource "aws_security_group" "lb" {
-  name = "${var.logical_name}-lb"
+  name        = "${var.logical_name}-lb"
   description = "controls access to the ALB"
-  vpc_id = "${data.aws_vpc.default.id}"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    protocol = "tcp"
+    protocol  = "tcp"
     from_port = 443
-    to_port = 443
+    to_port   = 443
     cidr_blocks = [
       "0.0.0.0/0"
     ]
   }
 
   ingress {
-    protocol = "tcp"
+    protocol  = "tcp"
     from_port = 443
-    to_port = 443
+    to_port   = 443
     ipv6_cidr_blocks = [
       "::/0"
     ]
   }
 
   ingress {
-    protocol = "tcp"
+    protocol  = "tcp"
     from_port = 80
-    to_port = 80
+    to_port   = 80
     cidr_blocks = [
       "0.0.0.0/0"
     ]
@@ -32,8 +32,8 @@ resource "aws_security_group" "lb" {
 
   egress {
     from_port = 0
-    to_port = 0
-    protocol = "-1"
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = [
       "0.0.0.0/0"
     ]
@@ -42,23 +42,25 @@ resource "aws_security_group" "lb" {
 
 # Traffic to the ECS Cluster should only come from the ALB
 resource "aws_security_group" "ecs_tasks" {
-  name = "${var.logical_name}"
+  name        = var.logical_name
   description = "allow inbound access from the ALB only"
-  vpc_id = "${data.aws_vpc.default.id}"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    protocol = "tcp"
-    from_port = "${var.port}"
-    to_port = "${var.port}"
+    protocol  = "tcp"
+    from_port = var.port
+    to_port   = var.port
     security_groups = [
-      "${aws_security_group.lb.id}"]
+      aws_security_group.lb.id
+    ]
   }
 
   egress {
-    protocol = "-1"
+    protocol  = "-1"
     from_port = 0
-    to_port = 0
+    to_port   = 0
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0"
+    ]
   }
 }
