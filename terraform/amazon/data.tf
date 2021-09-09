d6c7a718e5b710d6712a0515c667eb1d7959bd48
Shawn Bush <shawn.bush@soundcloud.com>
2021-09-08 17:19:06 -0700

    upgrade terraform version to 0.12.0

--- terraform/amazon/data.tf
+++ terraform/amazon/data.tf
@@ -1,38 +1,40 @@
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {
+}
 
 data "aws_vpc" "default" {
   default = true
 }
 
 data "aws_subnet_ids" "default" {
-  vpc_id = "${data.aws_vpc.default.id}"
+  vpc_id = data.aws_vpc.default.id
 }
 
 data "aws_subnet" "default" {
-  count = "${length(data.aws_subnet_ids.default.ids)}"
-  id    = "${data.aws_subnet_ids.default.ids[count.index]}"
+  count = length(data.aws_subnet_ids.default.ids)
+  id    = data.aws_subnet_ids.default.ids[count.index]
 }
 
 data "aws_ecs_cluster" "main" {
-  cluster_name = "${var.cluster_name}"
+  cluster_name = var.cluster_name
 }
 
 data "aws_iam_role" "task_container_role" {
-  name = "${var.ecs_task_container_role}"
+  name = var.ecs_task_container_role
 }
 
 data "aws_iam_role" "task_execution_role" {
-  name = "${var.ecs_task_execution_role}"
+  name = var.ecs_task_execution_role
 }
 
 data "aws_ecr_repository" "main" {
-  name = "${var.logical_name}"
+  name = var.logical_name
 }
 
 data "aws_route53_zone" "selected" {
-  name = "${local.aws_route53_zone_name}"
+  name = local.aws_route53_zone_name
 }
 
 data "aws_acm_certificate" "main" {
-  domain = "${local.domain_name}"
+  domain = local.domain_name
 }
+
