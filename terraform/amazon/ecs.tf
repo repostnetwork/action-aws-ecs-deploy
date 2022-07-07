2dc21315670ca6733fb6aa9f4977c6798830b971
Ian Brown <ian.brown@soundcloud.com>
2022-07-07 15:13:06 -0700

    try output

--- terraform/amazon/ecs.tf
+++ terraform/amazon/ecs.tf
@@ -42,11 +42,15 @@ resource "aws_ecs_task_definition" "efs" {
   }
 }
 
+output "aws_ecs_task_definition_arn" {
+  value = ${element(concat(aws_ecs_task_definition.main.arn, aws_ecs_task_definition.efs.arn, list("")), 0)}
+}
+
 resource "aws_ecs_service" "web" {
   count = var.is_worker ? 0 : 1 # no load balancer if worker
   name = var.logical_name
   cluster = data.aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition[0].arn
+  task_definition = output.aws_ecs_task_definition_arn
   desired_count = var.container_count
   launch_type = "FARGATE"
   health_check_grace_period_seconds = var.health_check_grace_period
@@ -79,7 +83,7 @@ resource "aws_ecs_service" "worker" {
   count = var.is_worker ? 1 : 0 # no load balancer if worker
   name = var.logical_name
   cluster = data.aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition[0].arn
+  task_definition = output.aws_ecs_task_definition_arn
   desired_count = var.container_count
   launch_type = "FARGATE"
   deployment_minimum_healthy_percent = 100
