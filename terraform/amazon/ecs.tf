18a7a2ac27bb6251e839bdd326a7013c588788fd
Ian Brown <ian.brown@soundcloud.com>
2022-07-07 15:59:45 -0700

    try locals

--- terraform/amazon/ecs.tf
+++ terraform/amazon/ecs.tf
@@ -42,15 +42,17 @@ resource "aws_ecs_task_definition" "efs" {
   }
 }
 
-output "aws_ecs_task_definition_arn" {
-  value = element(concat(aws_ecs_task_definition.main.*.arn, aws_ecs_task_definition.efs.*.arn, list("")), 0)
+locals {
+  aws_ecs_task_definition_arn {
+    value = element(concat(aws_ecs_task_definition.main.*.arn, aws_ecs_task_definition.efs.*.arn, list("")), 0)
+  }
 }
 
 resource "aws_ecs_service" "web" {
   count = var.is_worker ? 0 : 1 # no load balancer if worker
   name = var.logical_name
   cluster = data.aws_ecs_cluster.main.id
-  task_definition = output.aws_ecs_task_definition_arn
+  task_definition = local.aws_ecs_task_definition_arn.value
   desired_count = var.container_count
   launch_type = "FARGATE"
   health_check_grace_period_seconds = var.health_check_grace_period
@@ -83,7 +85,7 @@ resource "aws_ecs_service" "worker" {
   count = var.is_worker ? 1 : 0 # no load balancer if worker
   name = var.logical_name
   cluster = data.aws_ecs_cluster.main.id
-  task_definition = output.aws_ecs_task_definition_arn
+  task_definition = local.aws_ecs_task_definition_arn.value
   desired_count = var.container_count
   launch_type = "FARGATE"
   deployment_minimum_healthy_percent = 100
