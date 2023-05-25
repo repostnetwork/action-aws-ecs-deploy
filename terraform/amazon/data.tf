d9c0ef066f0dab69a6618aaa3413db230bdaffb4
Stirling Carter <stirling.h.carter@vanderbilt.edu>
2023-05-25 11:14:05 -0700

    updates depr resource identifier

--- terraform/amazon/data.tf
+++ terraform/amazon/data.tf
@@ -5,8 +5,11 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 data "aws_subnet" "default" {
