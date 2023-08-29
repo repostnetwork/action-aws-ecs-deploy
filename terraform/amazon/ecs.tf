8ee091c89ec586debf77354a8f70faf5b73509a0
Wing Lian <wing.lian@soundcloud.com>
2023-08-29 09:46:21 -0700

    explicit path to pgre

--- terraform/amazon/ecs.tf
+++ terraform/amazon/ecs.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "main" {
       }
     },
     "healthCheck": {
-      "command": ["CMD-SHELL", "pgrep java || exit 1"],
+      "command": ["CMD-SHELL", "/bin/pgrep java || exit 1"],
       "interval": 45,
       "timeout" : 5,
       "retries" : 3,
